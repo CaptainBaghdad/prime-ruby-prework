124f73f69a482c2a0b1c8153e6946e5701c90ab8
Jason Pitts <juniorjr083@gmail.com>
2018-06-05 00:33:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,5 @@
 # Add  code here!
 
-def prime?(num)
-  prime = true
-    for r in 2..Math.sqrt(num).to_i
-      if (num % r == 0)
-        prime = false
-        break
-      end
-    end
-    return prime
-  end
+def prime?(n)
+  (2..n/2).none?{|i| n % i == 0}
 end 
